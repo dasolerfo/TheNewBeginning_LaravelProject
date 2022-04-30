f4b201486acd1680174165256fba8d5823e16c76
Claudia23120 <claudia23120@hotmail.com>
2022-04-30 12:08:45 +0200

    Web

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 require('./bootstrap');
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
